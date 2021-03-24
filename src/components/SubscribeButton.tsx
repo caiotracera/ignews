f6d5388d6ebefdd74c2a172fefae4a31457d0faf
Caio Tracera <ctrcra@gmail.com>
2021-03-24 11:48:49 -0300

    feat: redirect to posts if user has an active subscription

--- src/components/SubscribeButton.tsx
+++ src/components/SubscribeButton.tsx
@@ -1,19 +1,37 @@
 import { signIn, useSession } from 'next-auth/client'
+import { Session } from 'next-auth'
+import { useRouter } from 'next/router'
 
 import { getStripeJs } from '@/services/stripe/public-stripe'
 import { api } from '@/services/api'
 import styles from '@/styles/components/subscribeButton.module.scss'
+
+interface SessionProps extends Session {
+  activeSubscription?: {
+    data: {
+      id: string
+      status: string
+      price_id: string
+    }
+  }
+}
 
 type SubscribeButtonProps = {
   priceId: string
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const [session] = useSession()
+  const [session]: [SessionProps, boolean] = useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github')
+      return
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
       return
     }
 

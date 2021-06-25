e4d7e139c3362afbfe1707e978c4609ee0d3f2a2
Caio Tracera <ctrcra@gmail.com>
2021-06-24 22:07:58 -0300

    test: add subscribe button test

--- src/components/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton.spec.tsx
@@ -8,9 +8,6 @@ import { SessionProps, SubscribeButton } from './SubscribeButton'
 jest.mock('next-auth/client')
 jest.mock('next/router')
 
-// useSession() {
-//   return [null, false]
-// },
 describe('SubscribeButton Component Test Suite', () => {
   it('renders correctly ', () => {
     const useSessionMocked = mocked(useSession)
@@ -37,14 +34,14 @@ describe('SubscribeButton Component Test Suite', () => {
     const useRouterMocked = mocked(useRouter)
     const useSessionMocked = mocked(useSession)
 
-    useSessionMocked.mockReturnValueOnce([
+    useSessionMocked.mockReturnValueOnce(([
       {
         user: { name: 'John Doe', email: 'john.doe@example.com' },
-        activeSubscription: 'fake-subscription', // fake error
+        activeSubscription: 'fake-subscription',
         expires: 'fake-expires'
       },
       false
-    ])
+    ] as unknown) as [SessionProps, boolean])
 
     const pushMock = jest.fn()
     useRouterMocked.mockReturnValueOnce({ push: pushMock } as any)
@@ -53,6 +50,6 @@ describe('SubscribeButton Component Test Suite', () => {
     const subscribeButton = screen.getByText('Subscribe now')
 
     fireEvent.click(subscribeButton)
-    expect(pushMock).toHaveBeenCalled()
+    expect(pushMock).toHaveBeenCalledWith('/posts')
   })
 })

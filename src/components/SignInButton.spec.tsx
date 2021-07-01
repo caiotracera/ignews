00044c29f54f18e402852150f987415e6e293315
Caio Tracera <ctrcra@gmail.com>
2021-07-01 14:23:01 -0300

    test: add test to sign in user

--- src/components/SignInButton.spec.tsx
+++ src/components/SignInButton.spec.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { mocked } from 'ts-jest/utils'
-import { useSession } from 'next-auth/client'
+import { useSession, signOut, signIn } from 'next-auth/client'
 
 import { SignInButton } from './SignInButton'
+import { SessionProps } from './SubscribeButton'
 
 jest.mock('next-auth/client')
 
@@ -30,5 +31,38 @@ describe('SignInButton Component Test Suite', () => {
     render(<SignInButton />)
 
     expect(screen.getByText('John Doe')).toBeInTheDocument()
+  })
+
+  it('sign out user when click button', () => {
+    const useSessionMocked = mocked(useSession)
+
+    const signOutMocked = mocked(signOut)
+    useSessionMocked.mockReturnValueOnce(([
+      {
+        user: { name: 'John Doe', email: 'john.doe@example.com' },
+        activeSubscription: 'fake-subscription',
+        expires: 'fake-expires'
+      },
+      false
+    ] as unknown) as [SessionProps, boolean])
+
+    render(<SignInButton />)
+    const signInButton = screen.getByText('John Doe')
+
+    fireEvent.click(signInButton)
+    expect(signOutMocked).toHaveBeenCalled()
+  })
+
+  it('sign in user when click button', () => {
+    const useSessionMocked = mocked(useSession)
+
+    const signInMocked = mocked(signIn)
+    useSessionMocked.mockReturnValueOnce([null, false])
+
+    render(<SignInButton />)
+    const signInButton = screen.getByText('Sign in with GitHub')
+
+    fireEvent.click(signInButton)
+    expect(signInMocked).toHaveBeenCalled()
   })
 })
